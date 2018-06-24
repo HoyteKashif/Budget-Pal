0596421fc13cf9ea312c5a4cdd3268d1564c5c0f
Kashif Hoyte <wiz@kash-home-debian>
2018-06-23 20:53:05 -0400

    Added url page mounting

--- source/com/TechTheEasyWay/page/WicketApplication.java
+++ source/com/TechTheEasyWay/page/WicketApplication.java
@@ -1,24 +1,28 @@
-package  com.TechTheEasyWay.page;
+package com.TechTheEasyWay.page;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
-public class WicketApplication extends WebApplication{
-	
-	public WicketApplication()
-	{
+public class WicketApplication extends WebApplication {
+
+	public WicketApplication() {
 		super();
 	}
-	
+
 	@Override
-	public Class<? extends Page> getHomePage()
-	{
+	public Class<? extends Page> getHomePage() {
 		return SignInPage.class;
 	}
 
 	@Override
-	public void init()
-	{
+	public void init() {
 		super.init();
+		mountPage("/SignIn", SignInPage.class);
+		mountPage("/CreateLedgerEntry", CreateNewLedgerEntryPage.class);
+		mountPage("/ViewLedgerInfo", ViewLedgerInfoPage.class);
+
+		/** New Pages (my own CSS and JavaScript) **/
+		mountPage("/LedgerInfo", com.TechTheEasyWay.page.ledger.ViewLedgerInfoPage.class);
+		mountPage("/CalendarPage", CalendarPage.class);
 	}
 }
