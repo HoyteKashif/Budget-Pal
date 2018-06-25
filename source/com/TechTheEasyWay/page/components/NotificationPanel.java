98cb5c44119778fb40b38c22425ddde7f85c5a33
Kashif Hoyte <Hoyte.Kashif@gmail>
2018-06-24 21:44:47 -0400

    Added class for he Feedback Panel.

---
+++ source/com/TechTheEasyWay/page/components/NotificationPanel.java
@@ -0,0 +1,12 @@
+package com.TechTheEasyWay.page.components;
+
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+
+public class NotificationPanel extends FeedbackPanel {
+	/** serialVersionUID **/
+	private static final long serialVersionUID = -7263686713421125240L;
+
+	public NotificationPanel(final String p_strId) {
+		super(p_strId);
+	}
+}
