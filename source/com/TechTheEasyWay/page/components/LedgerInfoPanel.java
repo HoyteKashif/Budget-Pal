fc8641e9e89eeaf266a2a7bba519ef1c536b4b81
Kashif Hoyte <Hoyte.Kashif@gmail>
2018-06-24 21:41:49 -0400

    Added functionality toallow for the deletion of a record if the delete link is clicked.

--- source/com/TechTheEasyWay/page/components/LedgerInfoPanel.java
+++ source/com/TechTheEasyWay/page/components/LedgerInfoPanel.java
@@ -1,53 +1,81 @@
 package com.TechTheEasyWay.page.components;
 
+import java.util.Objects;
+
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
 import com.TechTheEasyWay.bill.DB.LedgerDB;
 import com.TechTheEasyWay.bill.data.model.LedgerEntryModel;
-import com.TechTheEasyWay.page.CreateNewLedgerEntryPage;
+import com.TechTheEasyWay.page.ledger.CreateNewLedgerEntryPage;
 
-public class LedgerInfoPanel extends Panel 
-{
-	/**SerialUID*/
+public class LedgerInfoPanel extends Panel {
+	/** SerialUID */
 	private static final long serialVersionUID = 5706377213661434524L;
 
-	public LedgerInfoPanel( final String p_strId) {
-		
+	public LedgerInfoPanel(final String p_strId) {
+
 		super(p_strId);
-		final Form<Void> oFormLedgerTable  = new Form<Void>("formLedgerTable");
-		add( oFormLedgerTable);
-		
-		oFormLedgerTable.add (new Button("btnAddEntry")
-		{
-			/**SerialUID*/
+		add(getListViewOfLedgerEntries());
+		add(new Link<Void>("lnkAddEntry") {
+
+			/** SerialUID */
 			private static final long serialVersionUID = -8608318705083169618L;
 
 			@Override
-			public void onSubmit() {
+			public void onClick() {
 				setResponsePage(CreateNewLedgerEntryPage.class);
-			}	
-		});
-		
-		oFormLedgerTable.add (new ListView<LedgerEntryModel>("lstLedgerEntries", LedgerDB.getAllLedgerModels())
-		{
-			/**SerialUID */
-			private static final long serialVersionUID = -2965543685714247994L;
-
-			@Override
-			protected void populateItem(ListItem<LedgerEntryModel> p_oItem) 
-			{	
-				LedgerEntryModel oEntryModel = p_oItem.getModelObject();
-				p_oItem.add(new Label("strBillName", oEntryModel.getStrBillName()));
-				p_oItem.add(new Label("strAmountDue", "$" + ((oEntryModel.getlAmountDue() != null) ? oEntryModel.getlAmountDue() : "0")));
-				p_oItem.add(new Label("strDueDate", oEntryModel.getDueDate()));
-				p_oItem.add(new Label("strDatePaid", (oEntryModel.getDatePaid() != null) ? oEntryModel.getDatePaid() : "Not Paid Yet"));
-				p_oItem.add(removeLink("lnkDelete", p_oItem));
 			}
 		});
+	}
+
+	private ListView<LedgerEntryModel> getListViewOfLedgerEntries() {
+		return new ListView<LedgerEntryModel>("lstLedgerEntries", LedgerDB.getAllLedgerModels()) {
+			/** SerialUID */
+			private static final long serialVersionUID = -2965543685714247994L;
+
+			public Link<Void> dbRemoveLink(final String id, final ListItem<LedgerEntryModel> item) {
+				return new Link<Void>(id) {
+					private static final long serialVersionUID = 1L;
+
+					/**
+					 * @see org.apache.wicket.markup.html.link.Link#onClick()
+					 */
+					@Override
+					public void onClick() {
+						final LedgerEntryModel modelObject = item.getModelObject();
+
+						addStateChange();
+
+						item.modelChanging();
+
+						// Remove item and invalidate listView
+						getList().remove(item.getIndex());
+
+						modelChanged();
+						removeAll();
+
+						// delete it from the backing database
+						LedgerDB.delete(modelObject);
+					}
+				};
+			}
+
+			@Override
+			protected void populateItem(ListItem<LedgerEntryModel> p_oItem) {
+				final LedgerEntryModel oEntryModel = p_oItem.getModelObject();
+				p_oItem.add(new Label("strBillName",
+						Objects.nonNull(oEntryModel.getBill()) ? oEntryModel.getBill().getName() : ""));
+				p_oItem.add(new Label("strAmountDue",
+						"$" + (Objects.nonNull(oEntryModel.getAmountDue()) ? oEntryModel.getAmountDue() : "0")));
+				p_oItem.add(new Label("strDueDate", oEntryModel.getDueDate()));
+				p_oItem.add(new Label("strDatePaid",
+						(Objects.nonNull(oEntryModel.getDatePaid()) ? oEntryModel.getDatePaid() : "Not Paid Yet")));
+				p_oItem.add(dbRemoveLink("lnkDelete", p_oItem));
+			}
+		};
 	}
 }
