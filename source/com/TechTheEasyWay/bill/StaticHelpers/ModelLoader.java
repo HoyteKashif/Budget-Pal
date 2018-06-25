0c834ea928f3622735c50e3f751841fe9a525bed
Kashif Hoyte <Hoyte.Kashif@gmail>
2018-06-24 21:12:23 -0400

    Made changes to the naming of methods and method members.

--- source/com/TechTheEasyWay/bill/StaticHelpers/ModelLoader.java
+++ source/com/TechTheEasyWay/bill/StaticHelpers/ModelLoader.java
@@ -7,27 +7,30 @@ import com.TechTheEasyWay.bill.data.model.BillModel;
 import com.TechTheEasyWay.bill.data.model.LedgerEntryModel;
 
 public final class ModelLoader {
-	
-	private ModelLoader(){}
-	
-	public static LedgerEntryModel loadLedgerEntryModel(final int p_iBillId, final BigDecimal p_lAmountDue, final BigDecimal p_lMinimumPayment, final Date p_dtDueDate, final Date p_dtDatePaid, final BillModel p_oBillModel){
-		final LedgerEntryModel oEntryModel = new LedgerEntryModel();
-		oEntryModel.setAmountDue(p_lAmountDue);
-		oEntryModel.setMinimumPayment(p_lMinimumPayment);
-		oEntryModel.setDueDate(p_dtDueDate);
-		oEntryModel.setDatePaid(p_dtDatePaid);
-		oEntryModel.setBillModel(p_oBillModel);
-		return oEntryModel;
+
+	private ModelLoader() {
 	}
-	
-	public static BillModel loadBillModel( final int p_iBillId, final String p_strName, final BigDecimal p_lAmount, final int p_iDayDue, final BigDecimal p_bdBalance)
-	{
-		final BillModel oBillModel = new BillModel();
-		oBillModel.setBillId(p_iBillId);
-		oBillModel.setName(p_strName);
-		oBillModel.setAmount(p_lAmount);
-		oBillModel.setDueDate(p_iDayDue);		
-		oBillModel.setBalance(p_bdBalance);
-		return oBillModel;
+
+	public static LedgerEntryModel loadLedgerEntryModel(final int p_iBillId, final BigDecimal p_lAmountDue,
+			final BigDecimal p_lMinimumPayment, final Date p_dtDueDate, final Date p_dtDatePaid,
+			final BillModel p_oBillModel) {
+		final LedgerEntryModel oLedgerEntry = new LedgerEntryModel();
+		oLedgerEntry.setAmountDue(p_lAmountDue);
+		oLedgerEntry.setMinimumPayment(p_lMinimumPayment);
+		oLedgerEntry.setDueDate(p_dtDueDate);
+		oLedgerEntry.setDatePaid(p_dtDatePaid);
+		oLedgerEntry.setBillModel(p_oBillModel);
+		return oLedgerEntry;
+	}
+
+	public static BillModel loadBillModel(final int p_iBillId, final String p_strName, final BigDecimal p_lAmount,
+			final int p_iDayDue, final BigDecimal p_bdBalance) {
+		final BillModel oBill = new BillModel();
+		oBill.setId(p_iBillId);
+		oBill.setName(p_strName);
+		oBill.setAmount(p_lAmount);
+		oBill.setDueDate(p_iDayDue);
+		oBill.setBalance(p_bdBalance);
+		return oBill;
 	}
 }
